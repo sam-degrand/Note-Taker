76c72bd139101f03ac97e3657952443b9b2c5656
Sam DeGrandchamp <degrandchamps@gmail.com>
2023-06-20 20:20:11 -0400

    set up saving notes

--- server.js
+++ server.js
@@ -34,3 +34,15 @@ app.get("/api/notes/:id", function(req, res) {
 app.get("*", function(req, res) {
     res.sendFile(path.join(mainDir, "index.html"));
 });
+
+// Route to create a new note
+app.post("/api/notes", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    newNote.id = uuidv4(); // Generate a unique ID using uuidv4
+    savedNotes.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    console.log("Note saved to db.json. Content: ", newNote);
+    res.json(savedNotes);
+});
