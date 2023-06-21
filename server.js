3e2420343dab7924eb09c666c8e71c8174366ac3
Sam DeGrandchamp <degrandchamps@gmail.com>
2023-06-20 20:17:08 -0400

    added middleware, started setting app.get function

--- server.js
+++ server.js
@@ -8,3 +8,29 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const port = process.env.PORT || 3000;
 const mainDir = path.join(__dirname, "/public");
+
+// Set up middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Route to serve the notes page
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(mainDir, "notes.html"));
+});
+
+// Route to get all notes from the JSON file
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+// Route to get a specific note by ID
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
+});
+
+// Default route that serves the homepage
+app.get("*", function(req, res) {
+    res.sendFile(path.join(mainDir, "index.html"));
+});
